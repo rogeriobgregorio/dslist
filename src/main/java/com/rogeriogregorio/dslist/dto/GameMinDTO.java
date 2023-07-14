952b0aa987199df7eaa4800eb3478cac45bf9eb7
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2023-07-14 18:48:25 -0300

    Query SQL, projection, find games by list

--- src/main/java/com/rogeriogregorio/dslist/dto/GameMinDTO.java
+++ src/main/java/com/rogeriogregorio/dslist/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.rogeriogregorio.dslist.dto;
 
 import com.rogeriogregorio.dslist.entities.Game;
+import com.rogeriogregorio.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
 
@@ -19,6 +20,14 @@ public class GameMinDTO {
         year = entity.getYear();
         imgUrl = entity.getImgUrl();
         shortDescription = entity.getShortDescription();
+    }
+
+    public GameMinDTO(GameMinProjection projection) {
+        id = projection.getId();
+        title = projection.getTitle();
+        year = projection.getYear();
+        imgUrl = projection.getImgUrl();
+        shortDescription = projection.getShortDescription();
     }
 
     public Long getId() {
--- src/main/java/com/rogeriogregorio/dslist/services/GameService.java
+++ src/main/java/com/rogeriogregorio/dslist/services/GameService.java
@@ -3,6 +3,7 @@ package com.rogeriogregorio.dslist.services;
 import com.rogeriogregorio.dslist.dto.GameDTO;
 import com.rogeriogregorio.dslist.dto.GameMinDTO;
 import com.rogeriogregorio.dslist.entities.Game;
+import com.rogeriogregorio.dslist.projections.GameMinProjection;
 import com.rogeriogregorio.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -27,6 +28,14 @@ public class GameService {
     public List<GameMinDTO> findAll() {
 
         List<Game> result = gameRepository.findAll();
+        List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+        return dto;
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId) {
+
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
         List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
         return dto;
     }
