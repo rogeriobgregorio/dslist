5cd51fafa71cf7e0b25da17977e7c9a98796c77e
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2023-07-15 00:22:36 -0300

    Move games on list

--- src/main/java/com/rogeriogregorio/dslist/controllers/GameListController.java
+++ src/main/java/com/rogeriogregorio/dslist/controllers/GameListController.java
@@ -2,13 +2,11 @@ package com.rogeriogregorio.dslist.controllers;
 
 import com.rogeriogregorio.dslist.dto.GameListDTO;
 import com.rogeriogregorio.dslist.dto.GameMinDTO;
+import com.rogeriogregorio.dslist.dto.ReplacementDTO;
 import com.rogeriogregorio.dslist.services.GameListService;
 import com.rogeriogregorio.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -33,5 +31,11 @@ public class GameListController {
 
         List<GameMinDTO> result = gameService.findByList(listId);
         return result;
+    }
+
+    @PostMapping(value = "/{listId}/replacement")
+    public void move(@PathVariable Long listId, @RequestBody ReplacementDTO body) {
+
+        gameListService.move(listId, body.getSourceIndex(), body.getDestinationIndex());
     }
 }
